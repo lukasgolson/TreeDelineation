1ea5a7cf45e0e5b5bbc9990342ec07c1a68d9c1b
Lukas Olson <lukasdragon@live.ca>
2024-03-07 10:25:01 -0800

    First files

---
+++ main.R
@@ -0,0 +1,33 @@
+library(terra)
+
+library(lidR)
+
+
+las <- readLAS("tree_upright.las")
+las_check(las)
+
+las <- filter_duplicates(las)
+
+las <- decimate_points(las, random(1000))
+
+las <- classify_ground(las, algorithm = csf())
+
+dtm_tin <- rasterize_terrain(las, res = 1, algorithm = tin())
+
+chm <- rasterize_canopy(las, res = 1, algorithm = p2r())
+
+f <- function(x) {x * 0.1 + 3}
+heights <- seq(0,30,5)
+ws <- f(heights)
+
+
+ttops <- locate_trees(chm, algorithm = lmf(ws = 2.5))
+
+
+las <- segment_trees(las = las, algorithm = dalponte2016(chm = chm, treetops = ttops))
+
+
+length(unique(las$treeID) |> na.omit())
+
+
+writeLAS(las, "export.las")
